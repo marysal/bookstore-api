cd234e5a323a9481b05e5ae57db72ecc9947b875
Alex <a.verteiko1991@gmail.com>
2021-12-07 15:18:55 +0300

    project init

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=AuthorRepository::class)
+ * @ORM\Table(name="`authors`")
  */
 class Author
 {
@@ -17,8 +19,31 @@ class Author
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=256)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
     }
 }
