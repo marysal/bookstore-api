d786b43aee005634ffea177e0e8a06f6b6d36103
Alex <a.verteiko1991@gmail.com>
2021-12-17 18:51:42 +0300

    added show, update and destroy methods to OrdersController

--- src/Controller/Api/OrdersController.php
+++ src/Controller/Api/OrdersController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\Author;
 use App\Entity\Book;
 use App\Entity\Order;
 use App\Repository\OrderRepository;
@@ -130,5 +129,92 @@ class OrdersController extends AbstractController
         );
 
         return $this->json($jsonContent, Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/api/orders/{id}", name="app_api_order_show", methods={"GET"})
+     */
+    public function show(Order $order): Response
+    {
+        $jsonContent = $this->serializer->serialize(
+            [
+                'data' => $order
+            ],
+            'json',
+            [
+                'groups' => [
+                    'order',
+                    'book',
+                    'book_order'
+                ]
+            ]
+        );
+
+        return $this->json($jsonContent);
+    }
+
+    /**
+     * @Route("/api/orders/{id}", name="app_api_order_update", methods={"PUT"})
+     */
+    public function update(Request $request, Order $order): Response
+    {
+        $books = $request->get('books', []);
+
+        $order->setPhone($request->get('phone', ""));
+        $order->setAddress($request->get('address', ""));
+        $order->setStatus($request->get('status', ""));
+
+        if(empty($books)) {
+            throw $this->createNotFoundException('The order must contain at least one book ID');
+        }
+
+        foreach ($books as $bookId) {
+            $bookId = (int) $bookId;
+            $book = $this->entityManager->find(Book::class, $bookId);
+            $order->appendBookOrderList($book);
+        }
+
+        $errors = (string) $this->validator->validate($order);
+
+        if(!empty($errors)) {
+            throw $this->createNotFoundException($errors);
+        }
+
+        $this->entityManager->persist($order);
+        $this->entityManager->flush();
+
+        $jsonContent = $this->serializer->serialize(
+            [
+                'data' => $order
+            ],
+            'json',
+            [
+                'groups' => [
+                    'order',
+                    'book',
+                    'book_order'
+                ]
+            ]
+        );
+
+        return $this->json($jsonContent);
+    }
+
+    /**
+     * @Route("/api/orders/{id}", name="app_api_order_destroy", methods={"DELETE"})
+     */
+    public function destroy(Order $order): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($order);
+        $manager->flush();
+        $data = [
+            "errors" => false,
+            'message' => 'Deleted'
+        ];
+
+        $jsonContent = $this->serializer->serialize($data, 'json');
+
+        return $this->json($jsonContent);
     }
 }
