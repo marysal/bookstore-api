f2b88f8400f81383436c64cd7fef5ebed4addaa0
Alex <a.verteiko1991@gmail.com>
2021-12-28 21:16:40 +0300

    fixed ListBookTest

--- tests/BooksTest.php
+++ tests/BooksTest.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Entity\Author;
-use App\Entity\Book;
 use App\Repository\AuthorRepository;
-use App\Repository\BookRepository;
 
 class BooksTest extends BaseTest
 {
@@ -16,14 +14,6 @@ class BooksTest extends BaseTest
     protected static $authorId;
 
     protected $book;
-
-    //protected $booksCount;
-
-   /* protected function setUp(): void
-    {
-        parent::setUp();
-        $this->setBooksCount();
-    }*/
 
     public static function setUpBeforeClass(): void
     {
@@ -46,22 +36,4 @@ class BooksTest extends BaseTest
         $authors = $authorRepository->findOne();
         self::$authorId = $authors[0]->getId();
     }
-
-    /**
-     * @return int
-     */
-   /* public function getBooksCount(): int
-    {
-        return $this->booksCount;
-    }
-
-    private function setBooksCount()
-    {
-
-        $book = self::$client->getContainer()->get('doctrine')->getRepository(Book::class);
-        $this->booksCount = $book->createQueryBuilder('t')
-            ->select('count(t.id)')
-            ->getQuery()
-            ->getSingleScalarResult();
-    }*/
 }
