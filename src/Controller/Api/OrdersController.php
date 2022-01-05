5fd5cf6b536939c318760690d461574498dec1c0
Alex <a.verteiko1991@gmail.com>
2022-01-05 15:20:21 +0300

    Added ability to send request data and receive response in XML format for OrderController

--- src/Controller/Api/OrdersController.php
+++ src/Controller/Api/OrdersController.php
@@ -4,16 +4,17 @@ namespace App\Controller\Api;
 
 use App\Entity\Book;
 use App\Entity\Order;
-use App\Enum\EntityGroupsEnum;
 use App\Event\BeforeUpdateOrderEvent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class OrdersController extends BaseController
 {
+    protected $relationEntity = Book::class;
+    protected $entityName = "order";
+
     /**
      * @Route("/api/orders", name="app_api_orders_list", methods={"GET"})
      */
@@ -23,8 +24,9 @@ class OrdersController extends BaseController
 
         $orders = $this->orderRepository->findByFields($params);
 
-        return $this->json(
-            $this->getJsonContent($orders, EntityGroupsEnum::ENTITY_ORDERS)
+        return $this->response(
+            $orders,
+            $request->getAcceptableContentTypes()
         );
     }
 
@@ -34,29 +36,18 @@ class OrdersController extends BaseController
     public function create(Request $request): Response
     {
         $order = $this->serializer->deserialize($request->getContent(), Order::class, 'json');
-        $books = $request->get('books', []);
 
-        if(empty($books)) {
-            throw new HttpException(
-                Response::HTTP_BAD_REQUEST,
-                "The order must contain at least one book ID"
-            );
-        }
+        $books = $this->getIdsForLinkedTable($request);
 
-        foreach ($books as $bookId) {
-            $bookId = (int) $bookId;
-            $book = $this->entityManager->find(Book::class, $bookId);
-            $this->validate($book);
-            $order->appendBookOrderList($book);
-        }
+        $this->setEntityRelations($order, $books);
 
         $this->validate($order);
 
-        $this->entityManager->persist($order);
-        $this->entityManager->flush();
+        $this->saveToDb($order);
 
-        return $this->json(
-            $this->getJsonContent($order, EntityGroupsEnum::ENTITY_ORDERS),
+        return $this->response(
+            $order,
+            $request->getAcceptableContentTypes(),
             Response::HTTP_CREATED
         );
     }
@@ -64,10 +55,11 @@ class OrdersController extends BaseController
     /**
      * @Route("/api/orders/{id}", name="app_api_order_show", methods={"GET"})
      */
-    public function show(Order $order): Response
+    public function show(Request $request, Order $order): Response
     {
-        return $this->json(
-            $this->getJsonContent($order, "orders")
+        return $this->response(
+            $order,
+            $request->getAcceptableContentTypes()
         );
     }
 
@@ -82,48 +74,36 @@ class OrdersController extends BaseController
         $order = $this->serializer->deserialize(
             $request->getContent(),
             Order::class,
-            'json',
+            $request->getContentType(),
             [
                 AbstractNormalizer::OBJECT_TO_POPULATE => $order
             ],
         );
 
-        $books = $request->get('books', []);
+        $books = $this->getIdsForLinkedTable($request);
 
-        $this->validate($books);
-
-       /* if(empty($books)) {
-            throw $this->createNotFoundException('The order must contain at least one book ID');
-        }*/
-
-        foreach ($books as $bookId) {
-            $bookId = (int) $bookId;
-            $book = $this->entityManager->find(Book::class, $bookId);
-            $this->validate($book);
-            $order->appendBookOrderList($book);
-        }
+        $this->setEntityRelations($order, $books);
 
         $this->validate($order);
 
-        $this->entityManager->persist($order);
-        $this->entityManager->flush();
+        $this->saveToDb($order);
 
-        return $this->json(
-            $this->getJsonContent($order, EntityGroupsEnum::ENTITY_ORDERS)
+        return $this->response(
+            $order,
+            $request->getAcceptableContentTypes()
         );
     }
 
     /**
      * @Route("/api/orders/{id}", name="app_api_order_destroy", methods={"DELETE"})
      */
-    public function destroy(Order $order): Response
+    public function destroy(Request $request, Order $order): Response
     {
-        $manager = $this->getDoctrine()->getManager();
-        $manager->remove($order);
-        $manager->flush();
+        $this->saveToDb($order, "remove");
 
-        return $this->json(
-            $this->getJsonContent([], EntityGroupsEnum::ENTITY_DELETED)
+        return $this->response(
+            [],
+            $request->getAcceptableContentTypes()
         );
     }
 }
